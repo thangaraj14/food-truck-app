e409c595cff14e3f8a0ceedf6f28fc5d6be65cc8
Thangaraj A <thangaraj.a@sap.com>
2020-05-23 20:00:41 +0530

    refactoring the module

--- src/main/java/com/oracle/model/Resource.java
+++ src/main/java/com/oracle/model/Resource.java
@@ -7,15 +7,6 @@ import lombok.Getter;
 @Getter
 public class Resource {
 
-    private int dayorder;
-    private String dayofweekstr;
-    private String starttime;
-    private String endtime;
     private String location;
-    private String locationdesc;
-    private int locationid;
-    private String start24;
-    private String end24;
     private String applicant;
-
 }
